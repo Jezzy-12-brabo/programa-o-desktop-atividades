e97f015e58488536a2053acbace3c7f32093d86d
Jezzy-12-brabo <140173567+Jezzy-12-brabo@users.noreply.github.com>
2024-08-26 11:57:15 -0300

    Add files via upload

---
+++ CadastrodeMatricula/CadastrodeMatricula/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace CadastrodeMatricula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtnumero2 = new System.Windows.Forms.MaskedTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtestcivil = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtsexo = new System.Windows.Forms.ComboBox();
+            this.txtnumero1 = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtcpf = new System.Windows.Forms.TextBox();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtendereço = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtdata = new System.Windows.Forms.MaskedTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(526, 317);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 0;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.txtdata);
+            this.panel1.Controls.Add(this.txtnumero2);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.txtestcivil);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.txtsexo);
+            this.panel1.Controls.Add(this.txtnumero1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtcpf);
+            this.panel1.Controls.Add(this.btnDeletar);
+            this.panel1.Controls.Add(this.btnNovo);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.btnCadastrar);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtendereço);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtemail);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtnome);
+            this.panel1.Location = new System.Drawing.Point(50, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(693, 343);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.Location = new System.Drawing.Point(95, 170);
+            this.txtnumero2.Mask = "(99) 0000-0000";
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero2.TabIndex = 22;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 170);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Numero2";
+            // 
+            // txtestcivil
+            // 
+            this.txtestcivil.FormattingEnabled = true;
+            this.txtestcivil.Items.AddRange(new object[] {
+            "Solteiro",
+            "Casado"});
+            this.txtestcivil.Location = new System.Drawing.Point(317, 125);
+            this.txtestcivil.Name = "txtestcivil";
+            this.txtestcivil.Size = new System.Drawing.Size(121, 21);
+            this.txtestcivil.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(249, 128);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Estado Civil";
+            // 
+            // txtsexo
+            // 
+            this.txtsexo.FormattingEnabled = true;
+            this.txtsexo.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.txtsexo.Location = new System.Drawing.Point(318, 51);
+            this.txtsexo.Name = "txtsexo";
+            this.txtsexo.Size = new System.Drawing.Size(121, 21);
+            this.txtsexo.TabIndex = 17;
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.Location = new System.Drawing.Point(95, 132);
+            this.txtnumero1.Mask = "(99) 0000-0000";
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero1.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(249, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "CPF";
+            // 
+            // txtcpf
+            // 
+            this.txtcpf.Location = new System.Drawing.Point(318, 88);
+            this.txtcpf.Name = "txtcpf";
+            this.txtcpf.Size = new System.Drawing.Size(100, 20);
+            this.txtcpf.TabIndex = 14;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(607, 317);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletar.TabIndex = 13;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(445, 317);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 12;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(249, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sexo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Endereço";
+            // 
+            // txtendereço
+            // 
+            this.txtendereço.Location = new System.Drawing.Point(318, 15);
+            this.txtendereço.Name = "txtendereço";
+            this.txtendereço.Size = new System.Drawing.Size(100, 20);
+            this.txtendereço.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Numero1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "E-mail";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(95, 91);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(100, 20);
+            this.txtemail.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Idade";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(95, 15);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(100, 20);
+            this.txtnome.TabIndex = 0;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(668, 420);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 23;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(2, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(794, 59);
+            this.panel2.TabIndex = 24;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.Control;
+            this.label10.Location = new System.Drawing.Point(282, 13);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(205, 29);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Matricula Alunos";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // txtdata
+            // 
+            this.txtdata.Location = new System.Drawing.Point(95, 54);
+            this.txtdata.Mask = "000";
+            this.txtdata.Name = "txtdata";
+            this.txtdata.Size = new System.Drawing.Size(100, 20);
+            this.txtdata.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Matricula";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtendereço;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtcpf;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.ComboBox txtestcivil;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox txtsexo;
+        private System.Windows.Forms.MaskedTextBox txtnumero1;
+        private System.Windows.Forms.MaskedTextBox txtnumero2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.MaskedTextBox txtdata;
+    }
+}
+
---
+++ CadastrodeMatricula/CadastrodeMatricula/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastrodeMatricula
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void HabilitarBotao()
+        {
+            btnDeletar.Enabled = true;
+            btnCadastrar.Enabled = true;
+        }
+
+
+        private void LimparCampos()
+        {
+            txtnumero1.Text = "";
+            txtnumero2.Text = "";
+            txtcpf.Text = "";
+            txtdata.Text = "";
+            txtemail.Text = "";
+            txtnome.Text = "";
+            txtendereço.Text = "";
+            txtestcivil.Text = "";
+            txtsexo.Text = "";
+
+        }
+
+        private void DesabilitarCampos()
+        {
+            txtnome.Enabled = false;
+            txtnumero1.Enabled = false;
+            txtemail.Enabled = false;
+            txtnumero2.Enabled = false;
+            txtcpf.Enabled = false;
+            txtdata.Enabled = false;
+            txtendereço.Enabled = false;
+            txtestcivil.Enabled = false;
+            txtsexo.Enabled= false;
+        }
+
+        private void HabilitarCampos()
+        {
+            txtnome.Enabled = true;
+            txtnumero1.Enabled = true;
+            txtemail.Enabled = true;
+            txtnumero2.Enabled = true;
+            txtcpf.Enabled = true;
+            txtdata.Enabled = true;
+            txtendereço.Enabled = true;
+            txtestcivil.Enabled = true;
+            txtsexo.Enabled = true;
+
+
+        }
+
+        private void DesabilitarBotao()
+        {
+            //btnnovo.Enabled = false;
+            btnCadastrar.Enabled = false;
+            btnDeletar.Enabled = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            HabilitarCampos();
+            LimparCampos();
+            HabilitarBotao();
+            btnNovo.Enabled = false;
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            DesabilitarCampos();
+            LimparCampos();
+            DesabilitarBotao();
+            btnNovo.Enabled = true;
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            btnCadastrar.Enabled = false;
+            btnDeletar.Enabled = false;
+            btnNovo.Enabled = true;
+            LimparCampos() ;
+            DesabilitarBotao();
+            DesabilitarCampos();
+           
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
